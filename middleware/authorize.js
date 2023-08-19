45586fe2b7b48eb4e395ac63602593b6dfb163fb
jblackman42 <jblackman@pureheart.org>
2023-08-18 17:57:39 -0700

    removed unused stuff

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -141,51 +141,8 @@ const ensurePrayerLeaderByID = async (req, res, next) => {
     }
 }
 
-const checkUserGroups = async (req, res, next) => {
-    const data = await axios({
-        method: 'post',
-        url: `${process.env.BASE_URL}/oauth/connect/token`,
-        data: qs.stringify({
-            grant_type: "client_credentials",
-            scope: "http://www.thinkministry.com/dataplatform/scopes/all",
-            client_id: process.env.CLIENT_ID,
-            client_secret: process.env.CLIENT_SECRET
-        })
-    })
-        .then(response => response.data)
-    const {access_token} = data;
-
-    const groupUserIds = await axios({
-        method: 'get',
-        url: `${process.env.BASE_URL}/tables/dp_User_User_Groups?$filter=User_Group_ID=${process.env.AUTHORIZED_USER_GROUP_ID}`,
-        headers: {
-            'authorization': `Bearer ${access_token}`
-        }
-    })
-        .then(response => response.data.map(user => parseInt(user.User_ID)))
-        .catch(err => {
-            console.log('something went wrong: ' + err);
-            res.render('pages/login', {error: 'internal server error'});
-        })
-
-    const {user} = req.session;
-    // checks authorized user group for logged in user's id
-    const userAuthorized = groupUserIds.filter(id => id == user.userid).length > 0 || (user.roles && user.roles.includes("Administrators"));
-    
-    // if user IS a part of authorized group
-    // let em in
-    if (userAuthorized) return next();
-    // if user is not a part of authorized group
-    // kick em out
-    req.session.user = null;
-    req.session.access_token = null;
-    req.session.refresh_token = null;
-    return res.render('pages/login', {error: 'unauthorized'})
-}
-
 module.exports = {
     ensureAuthenticated,
-    checkUserGroups,
     ensurePrayerLeader,
     ensurePrayerLeaderByID
 }
